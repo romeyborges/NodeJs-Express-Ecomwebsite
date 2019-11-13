22a6c81f5317ad68560fed88d3aaf0dac1237539
romeyborges <romeyborges3@gmail.com>
2019-11-14 00:10:17 +0300

    Signup view, csrf and user model

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
+var session = require('express-session');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret:'mysecret', resave: false, saveUninitialized: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf =require('csurf');
 
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Product.find(function(err, docs){
-    var productChuncks = [];
-    var chunckSize = 3;
-    for( var i = 0; i < docs.length; i += chunckSize) {
-        productChuncks.push(docs.slice(i, i + chunckSize));
-    }
-    res.render('shop/index', { title: 'Shopping Cart', products: productChuncks});
-  });
+    Product.find(function(err, docs){
+      var productChuncks = [];
+      var chunckSize = 3;
+      for( var i = 0; i < docs.length; i += chunckSize) {
+          productChuncks.push(docs.slice(i, i + chunckSize));
+      }
+      res.render('shop/index', { title: 'Shopping Cart', products: productChuncks});
+    });
+});
+//creating route for sign up page
+router.get('/user/signup', function(req, res, next) {
+  res.render('user/signup', {csrfToken: req.csrfToken()});
 });
 
+router.post('/user/signup', function(req, res, next) {
+    res.redirect('/');
+})
 
 module.exports = router;

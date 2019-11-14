096f5fd156d17404a79dcca9f69cce0d7ff99c4b
romeyborges <romeyborges3@gmail.com>
2019-11-14 03:08:16 +0300

    SignUp page with authentication & encryption keys

--- app.js
+++ app.js
@@ -2,25 +2,36 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var routes = require('./routes/index');
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/shopDB');
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', 'hbs');
-
+//Initializing dependancies
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret:'mysecret', resave: false, saveUninitialized: false }))
+app.use(session({secret:'mysecret', resave: false, saveUninitialized: false }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
